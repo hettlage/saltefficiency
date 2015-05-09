06000a3e6fd3f3c38a866c9d634daf28b03b8aae
Christian Hettlage <hettlage@saao.ac.za>
2015-05-09 15:44:58 +0200

    Changed version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ for line in readme:
     readme_text += line
 
 setup(name='salt_efficiency',
-      version='0.2.1',
+      version='0.2.2',
       author='Paul Kotze',
       author_email='jpk@saao.ac.za',
       url='http://astronomer.salt.ac.za',
