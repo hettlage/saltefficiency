8204ea93d0690d16027559db6ec79e8a33d67ac0
Christian Hettlage <hettlage@saao.ac.za>
2020-05-09 20:48:54 +0200

    don't sort values

--- saltefficiency/plot/bokeh_plots.py
+++ saltefficiency/plot/bokeh_plots.py
@@ -63,8 +63,8 @@ def pie_chart(values, categories, colors, title, plot_width, legend_width, text_
         if v < 0:
             raise ValueError('values must be non-negative: {0}'.format(v))
 
-    # sort categories by decreasing value
-    values, categories = map(list, zip(*sorted(zip(values, categories), reverse=True)))
+    # # sort categories by decreasing value
+    # values, categories = map(list, zip(*sorted(zip(values, categories), reverse=True)))
 
     # convert values to angles and percentages
     angles = normalize(values, 2 * math.pi)
